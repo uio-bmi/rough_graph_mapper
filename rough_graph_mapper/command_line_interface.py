3edada53c42cf9a65a6dff498a6ba6b35b345a80
Ivar Grytten <ivar.grytten@gmail.com>
2019-01-12 23:37:46 +0100

    Initial filtering of graphalignments

--- rough_graph_mapper/command_line_interface.py
+++ rough_graph_mapper/command_line_interface.py
@@ -3,7 +3,12 @@ import argparse
 import sys
 from .linear_to_graph_mapper import LinearToGraphMapper
 from .traverse_mapper import TraverseMapper
+from .filter_graphalignments import filter_graphalignments
 
+def run_filter(args):
+    alignments = args.alignments
+    min_mapq = args.min_mapq
+    filter(alignments, min_mapq)
 
 def run_map_linear_to_graph(args):
     chromosomes = args.chromosomes.split(",")
@@ -26,6 +31,7 @@ def run_argument_parser(args):
     subparsers = parser.add_subparsers(help='Subcommands')
     subparser_linear = subparsers.add_parser("map_linear_to_graph", help="...")
     subparser_graph = subparsers.add_parser("traversemapper", help="...")
+    subparser_filter = subparsers.add_parser("filter", help="....")
 
     for subparser in [subparser_linear, subparser_graph]:
         subparser.add_argument("-r", "--reference", help="Linear reference fasta", required=True)
@@ -35,8 +41,13 @@ def run_argument_parser(args):
 
     subparser_graph.add_argument("-s", "--skip_run_linear", default=False, type=bool, required=False)
 
+    subparser_filter.add_argument("-m", "--min-mapq", help="Minimum mapq to keep")
+    subparser_filter.add_argument("-a", "--alignments", help="Graphalignments file to filter")
+
     subparser_linear.set_defaults(func=run_map_linear_to_graph)
     subparser_graph.set_defaults(func=run_graphtraverser)
+    subparser_filter.set_defaults(func=run_filter)
+
 
     if len(args) == 0:
         parser.print_help()
---
+++ rough_graph_mapper/filter_graphalignments.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+import logging
+
+
+def filter_graphalignments(file_name, min_mapq=50):
+    f = open(file_name)
+    alignments = defaultdict(list)  # read id to list of alignments
+
+    for i, line in enumerate(f):
+        if i % 1000000 == 0:
+            logging.info("Read %d lines" % i)
+        l = line.split("\t")
+        alignments[l[0]].append((l[1], int(l[2])))
+
+    n_single = 0
+    n_skipped_multiple_good = 0
+    n_skipped_multiple_good_unique = 0
+    n_kept_multiple_good = 0
+    n_kept_multiple_good_unique = 0
+    n_filtered_bad_score = 0
+    n_filtered_linear_many_alternative = 0
+
+    for read_name, hits in alignments.items():
+        sorted_hits = sorted(hits, key=lambda x: x[1])
+        if sorted_hits[0][1] >= 5:
+            # Skip because too bad score
+            n_filtered_bad_score += 1
+            continue
+
+        if len(sorted_hits) == 1:
+            best_hit = sorted_hits[0]
+            score = 60 - best_hit[1] * 2
+            n_single += 1
+        else:
+            # Only keep if second best has much more mismatches
+            n_mismatches = sorted_hits[0][1]
+            if sorted_hits[1][1] <= n_mismatches + 3:
+                # skip
+                n_skipped_multiple_good_unique += 1
+                n_skipped_multiple_good += len(sorted_hits)
+                continue
+            else:
+                best_hit = sorted_hits[0]
+                n_kept_multiple_good_unique += 1
+                n_kept_multiple_good += len(sorted_hits)
+
+            score = 5
+            if len(hits) == 2:
+                score = 40
+            elif len(hits) == 3:
+                score = 30
+            elif len(hits) >= 4:
+                score = 5
+
+            score -= best_hit[1] * 2 + (sorted_hits[1][1] - sorted_hits[0][1])
+
+        if score >= min_mapq:
+            print("%s\t%s\t%s" % (read_name, best_hit[0], score))
+
+    logging.info("%d reads filtered because multimapping on linear ref" % n_filtered_linear_many_alternative)
+    logging.info("%d reads filtered because too low score" % n_filtered_bad_score)
+    logging.info("%d reads have only a single good alignment" % n_single)
+    logging.info("%d reads filtered out because they have multiple good mappings (%d in total)" %
+                 (n_skipped_multiple_good_unique, n_skipped_multiple_good))
+    logging.info("%d reads with multiple alignments kept, because second best is bad (%d in total)" %
+                 (n_kept_multiple_good_unique, n_kept_multiple_good))
+
+
