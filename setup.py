8c2fa8d26c427c501b5068c9efd86405f868e2cd
Ivar Grytten <ivar.grytten@gmail.com>
2019-02-20 20:46:35 +0100

    Dependency fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='rough_graph_mapper',
       author_email='',
       license='MIT',
       zip_safe=False,
-      install_requires=['numpy', 'python-coveralls',
+      install_requires=['numpy', 'python-coveralls', 'scikit-bio', 'pysam',
                         'pyfaidx', 'offsetbasedgraph==2.1.3', 'pyvg', 'graph_peak_caller', 'tqdm'],
       classifiers=[
             'Programming Language :: Python :: 3'
