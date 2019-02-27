4b1ce7e1dbc64be2a0dddcc1d6f8d388cb15045d
Ivar Grytten <ivar.grytten@gmail.com>
2019-02-27 14:31:02 +0100

    Added pygssw as dependency

--- setup.py
+++ setup.py
@@ -8,13 +8,11 @@ setup(name='rough_graph_mapper',
       author_email='',
       license='MIT',
       zip_safe=False,
-      install_requires=['numpy', 'python-coveralls', 'scikit-bio', 'pysam', 'mappy',
+      install_requires=['numpy', 'pygssw', 'python-coveralls', 'scikit-bio', 'pysam', 'mappy',
                         'pyfaidx', 'offsetbasedgraph==2.1.3', 'pyvg', 'graph_peak_caller', 'tqdm'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
       entry_points = {
         'console_scripts': ['rough_graph_mapper=rough_graph_mapper.command_line_interface:main'],
-      }
-
-      )
+      })
